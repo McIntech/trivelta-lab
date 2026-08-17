9518b6e79062b6622ec95028a466126c80f21e6d
Franco Limón <francoenriquee5@gmail.com>
2026-08-16 18:45:58 -0600

    chore: initial project setup

---
+++ backend/app/db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://franco@localhost:5432/trivelta_lab"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
